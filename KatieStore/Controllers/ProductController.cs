908669e9b8255e5a5edc1d86ad7818b1e6f60f30
kcjacob <kcjacob90@gmail.com>
2017-05-22 14:32:25 -0500

    Add project files.

---
+++ KatieStore/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KatieStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+       /* public ActionResult Index(in? id)
+        {
+            if(id == 300)
+            {
+                return this.Redirect("/");
+        
+            }
+            var product = new { id = id, name = "My Product", price = 299m, description = "This is a product" };
+            return Json(product, JsonRequestBehavior.AllowGet);
+        }*/
+
+        [HttpPost]
+        public ActionResult Index(int? id)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
